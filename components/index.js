d690226a4513c483b7e77b22d233758b64d7f786
Ritesh Kumar <rkritesh078@gmail.com>
2017-06-19 17:10:09 +0530

    feat(setup): testing working

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import { View, Text } from 'react-native';
+
+export default class MyComponent extends PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>I wonder if there will be any problems...</Text>
+      </View>
+    );
+  }
+}
---
+++ tests/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { shallow } from 'enzyme';
+import MyComponent from '../components';
+import { expect } from 'chai';
+
+describe('<MyComponent />', () => {
+  it('should render text content', () => {
+    const wrapper = shallow(<MyComponent />);
+    expect(wrapper.length).to.equal(1);
+    expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
+  });
+});
